dbc184e159a1eb29c608004f212db416a3540bf8
parthiban.n <parthiban.n@realimage.com>
2014-04-09 13:21:16 +0000

    added sample test cases

---
+++ Test/Modules/Module1/functionality1_tests.py
@@ -0,0 +1,30 @@
+import sys
+import unittest
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__),'../../../'))
+
+class functionality1(unittest.TestCase):
+    """ Explain about the functionality here
+    """
+    def setUp(self):
+        """ setup required for running testcases.
+        """
+    
+    def tearDown(self):
+        """ clear the objects that are created in the setup.
+        """
+        
+    def test_testcase1(self):
+        """ Explain about testcase1 here.
+        I'm simply passing the test case here
+        """
+        pass
+    
+    def test_testcase2(self):
+        """ Explain about testcase2 here.
+        I'm simply failing the test case here
+        """
+        self.fail("failed to demonstrate test failure.")
+        
+if __name__ == '__main__':
+    unittest.main()
--- TestManager.py
+++ TestManager.py
@@ -1,6 +1,21 @@
+from Test.Modules.Module1 import functionality1_tests
+from Test.Modules.Module1 import functionality2_tests
+from Test.Modules.Module2 import functionality3_tests
+from Test.Modules.Module2 import functionality4_tests
+
 class TestManager():
     """ To manage all the TestSuites
     """
     def TestSuites(self):
         """All the TestSuites are called here and returned as a list. 
         """
+        module1 = []
+        module1.append(functionality1_tests.functionality1)
+        module1.append(functionality2_tests.functionality2)
+        
+        module2 = []
+        module2.append(functionality3_tests.functionality3)
+        module2.append(functionality4_tests.functionality4)
+        
+        testSuites = module1 + module2
+        return testSuites
