c7e6d2cf4e2b71f3f028eacaa82ef99dc68135b9
parthibann <parthiban.n@realimage.com>
2014-04-15 20:21:12 +0530

    Modified testSelector tmplate for testlink support
    1. In HTML TestSelector template added provision to get userid,testplanid and buildname.
    2. Clicking on the HTMLTestRunner radio button hides the TLRunner div and clicking on the TestLinkRunner Radio button displays the TLRunner div.

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ class SelectTestCasesHTMLMaker():
         <script src="http://code.jquery.com/jquery-1.9.1.js" type="text/javascript"></script>
         <script type="text/javascript">
             $(function () {
+              $(".runnerRadio").click(function(){if($(this).val()==="TestLinkRunner") $("#TLRunner").show("fast"); else $("#TLRunner").hide("fast");})
               $('.RunSelectedTests').click( function(){
                 var chkId = '';
                 $('.chkTest:checked').each(function() {
@@ -91,7 +92,10 @@ class SelectTestCasesHTMLMaker():
                 });
                 testCases = chkId;
 		var selectedRunner = $('input[name=testRunner]:radio:checked').val()
-                $.ajax({type:'POST',url:"http://"""+self.serverIpAddress+""":"""+self.port+"""/runtest",data : JSON.stringify({"testCases":testCases,"Runner":selectedRunner}),dataType: "json",success:function(){alert('success')},error: function(e){alert(JSON.stringify(e))},contentType: "application/json; charset=utf-8"});
+		var userId = $('input:text[name=useridtxtbox]').val();
+		var testPlanId = $('input:text[name=testplantxtbox]').val();
+		var buildName = $('input:text[name=buildnametxtbox]').val();
+                $.ajax({type:'POST',url:"http://"""+self.serverIpAddress+""":"""+self.port+"""/runtest",data : JSON.stringify({"testCases":testCases,"Runner":selectedRunner,"userId":userId,"testPlanId":testPlanId,"buildName":buildName}),dataType: "json",success:function(){alert('success')},error: function(e){alert(JSON.stringify(e))},contentType: "application/json; charset=utf-8"});
               });
               $('.selectAllTestCases').click( function(){
                 $('.chkTest').prop('checked',$(this).is(':checked'));
@@ -118,9 +122,13 @@ class SelectTestCasesHTMLMaker():
         HTMLPart4 = self.makeTestCases()
         
         HTMLPart5 = """</div>
+        <div id="TLRunner" style="background-color:"""+self.theme+""";font-weight:bold;text-align:center;width:100%;display:none;">User Id:
+	<input type="text" name="useridtxtbox" placeholder="Enter User Id *" size=14>TestPlan Id :
+	<input type="text" name="testplantxtbox" placeholder="Enter TestPlan Id *" size=14>Build Name :
+	<input type="text" name="buildnametxtbox" placeholder="Enter Build Name *" size=14></div>
         <div id="runner" style="background-color:"""+self.theme+""";font-weight:bold;text-align:center;width:100%;">Select Runner : 
-        <input type="radio" name="testRunner" value="HTMLTestRunner" checked>HTMLTestRunner
-        <input type="radio" name="testRunner" value="TestLinkRunner">TestLinkRunner
+        <input type="radio" name="testRunner" class="runnerRadio" value="HTMLTestRunner" checked>HTMLTestRunner
+        <input type="radio" name="testRunner" class="runnerRadio" value="TestLinkRunner">TestLinkRunner
         <input type="checkbox" class="selectAllTestCases" />Select All Test Cases
         <button type="button" class="RunSelectedTests">Run</button></div>
         </div>
